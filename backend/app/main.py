4d1d81f63b4d5f0dc34b20f690eff66ece492971
socialgenius-au <socialgenius.au@gmail.com>
2026-06-14 20:11:10 +1000

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from app.database import engine, Base
-from app.routers import merchants, products, happyhours, orders, payments, auth, promo, admin
+from app.routers import merchants, products, happyhours, orders, payments, auth, promo, admin, tasks
 import app.models.models as models
 import os
 
